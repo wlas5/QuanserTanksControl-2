828c87cdf137be3489253dc88c23ef97775e719b
wlas5 <87370007+wlas5@users.noreply.github.com>
2023-07-20 15:48:59 -0300

    Com interface gráfica
    Adicionados os gráficos em tempo real, mudança de referência e mais de um tipo de controle em malha fechada

--- closed_loop_v2.py
+++ closed_loop_v2.py
@@ -8,19 +8,28 @@ import os
 import ifb_arte
 import smart_datalog
 from io_quanser import leia, desligar_bomba, aplica_controle, trava
+import matplotlib.pyplot as plt
+
 
 referencia = 20
 #nivel_tanque_2 = 0.0
 
 malha_fechada = True
+pid = True
+mudar_referencia = True
 sinal_malha_aberta = 3
 save_data = True
+
+if malha_fechada and pid:
+    on_off = False
+if malha_fechada and not pid:
+    on_off = True
 
 kp = 1
 ki = 1.5
 
 # Intervalo de amostragem 
-sampleTime = 0.3
+sampleTime = 0.4
 
 
 card = HIL()
@@ -34,24 +43,50 @@ def main():
         cabecalho = "Hora;referencia;nivel_tanque1;nivel_tanque2;sinal_controle;sinal_aplicado\n"
         smart_datalog.criar_datalog(cabecalho)
 
+        # Preparar o gráfico
+        plt.ion()  # Modo de plotagem interativo
+        fig, ax = plt.subplots()
+        x_vals = []
+        y_vals_tanque1 = []
+        y_vals_tanque2 = []
+        y_vals_referencia = []
+        y_vals_sinal_controle = []
+        line_tanque1, = ax.plot(x_vals, y_vals_tanque1, label='Tanque 1')
+        line_tanque2, = ax.plot(x_vals, y_vals_tanque2, label='Tanque 2')
+        line_referencia, = ax.plot(x_vals, y_vals_referencia, label='Referencia')
+        line_sinal_controle, = ax.plot(x_vals, y_vals_sinal_controle, label='Sinal de Controle')
+        ax.legend()
+
         while True:
             print("")
             print(f'{date.today().strftime("%d/%m/%y")}\n')
+            print("")
+            print(f'Tempo de execução: {time.perf_counter()}')
             startTime = time.perf_counter()
-            
             start = datetime.now()
+            if time.perf_counter() > 30:
+                global referencia
+                referencia = 10
 
             #leitura das variáveis
             nivel_tanque_1, nivel_tanque_2, corrente_bomba = leia(card)
+
 
             #controle 
             erro = referencia - nivel_tanque_2
 
             if malha_fechada:
-                u = kp * erro + ki * erro_ant * sampleTime
-                #u = kp * erro
-                erro_ant += erro 
-                erro_ant = max(min(erro_ant,3),-3) #limita a parcela integrativa entre -3 e 3
+                if pid:
+                    u = kp * erro + ki * erro_ant * sampleTime
+                    #u = kp * erro
+                    erro_ant += erro 
+                    erro_ant = max(min(erro_ant,3),-3) #limita a parcela integrativa entre -3 e 3
+                if on_off:  
+                    if erro > 1:
+                        u = 4
+                    elif erro < -1:
+                        u =-4
+                        
             else:
                 u = sinal_malha_aberta
 
@@ -68,6 +103,28 @@ def main():
             if save_data:
                 data = f"{referencia};{nivel_tanque_1:.2f};{nivel_tanque_2:.2f};{u:.2f};{aplica_controle(card,u):.2f}"
                 smart_datalog.salvar_dados(data)
+
+            # Atualizar o gráfico
+            x_vals.append(len(x_vals))
+            y_vals_tanque1.append(nivel_tanque_1)
+            y_vals_tanque2.append(nivel_tanque_2)
+            y_vals_referencia.append(referencia)
+            y_vals_sinal_controle.append(aplica_controle(card,u))
+            line_tanque1.set_xdata(x_vals)
+            line_tanque1.set_ydata(y_vals_tanque1)
+            line_tanque2.set_xdata(x_vals)
+            line_tanque2.set_ydata(y_vals_tanque2)
+            line_referencia.set_xdata(x_vals)
+            line_referencia.set_ydata(y_vals_referencia)
+            line_sinal_controle.set_xdata(x_vals)
+            line_sinal_controle.set_ydata(y_vals_sinal_controle)
+            ax.relim()
+            ax.autoscale_view()
+
+            plt.draw()
+            plt.pause(0.001)
+
+        
 
             #rotina de controle de tempo: 
             endTime = time.perf_counter()
